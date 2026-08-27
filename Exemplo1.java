9b1099ba294bed2c4ac98afe0107719ac10fbec1
jm4787750-create <jm4787750@gmail.com>
2026-08-27 08:51:12 -0300

    Add files via upload

---
+++ Exemplo1.java
@@ -0,0 +1,15 @@
+public class Exemplo1 {
+    public static void main (String[] args){
+
+    System.out.print("esse algoritmo ira listar os meus 5 livros favoritos.");
+    System.out.println("Livros 1:O hobbit");
+    System.out.println("Livro 2: turma da monica");
+    System.err.println("Livro 3: blibia");
+    System.err.println("Livros 4: Odisseia");
+    System.err.println("Livros 5: NARUTO ");
+
+    }
+
+}
+    
+
---
+++ atividade1.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+
+public class atividade1 {
+    public static void main (String[]args){
+
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Cadastro dos 5 Livros favoritos");
+        System.out.println("");
+
+        System.out.println("Digite seu livro favorito 1: ");
+        String Livros1 = entrada.nextLine();
+
+        System.out.println("Digite seu livro favorito 2: ");
+        String Livros2 = entrada.nextLine ();
+
+        System.out.println("Digite seu livro favorito 3: ");
+        String livros3 = entrada.nextLine();
+
+        System.err.println("Digite seu livro favorito 4: ");
+        String Livros4 = entrada.nextLine();
+
+        System.err.println("Digite seu livro favorito 5: ");
+        String Livros5 = entrada.nextLine();
+
+
+
+
+    }
+    
+}
